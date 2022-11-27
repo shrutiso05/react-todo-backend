f6125df27770361ab8ee4036dea14b004b320c0c
shrutiso05 <shristisoni051199@gmail.com>
2022-11-27 18:10:39 +0530

    third commit

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2z^#+1urp4a6p$omr4380zyq8dato0mf#qxt2r0!jpfxqj#d4h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['reacttododja.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [ ]
 
 
 # Application definition
